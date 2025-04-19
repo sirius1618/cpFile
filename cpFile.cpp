84a9d5cbded9fccc06cb50c696f09ae2d55cb765
Felipe <sagittarius_a1.618@proton.me>
2025-04-19 19:27:09 -0300

    feat: adicionado logica para texto do arquivo ir para area de trasferencia

--- cpFile.cpp
+++ cpFile.cpp
@@ -1,3 +1,22 @@
-#include
-#include
-#include
+#include <iostream> 
+#include <cstdlib>
+
+using namespace std;
+
+void copiarClipBord(const string& file) {
+  string comando = "cat " + file +" | xclip -selection clipboard";
+  system(comando.c_str());
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) { 
+      cout << "Uso: " << argv[0] << " <arquivo>" << endl; 
+      cout << "Exemplo: " << argv[0] << " programa.py" << endl;
+      return 1;
+   }
+
+  string caminhoFile = argv[1];
+  copiarClipBord(caminhoFile);
+  cout << "Arquivo copiado!" << endl;
+  return 0;
+}
